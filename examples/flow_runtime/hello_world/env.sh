3a5b5b0cee220f83b26aa4643820ba3d76c0b44a
Jiajin Lin <Jiajin.Lin@ibm.com>
2025-03-31 18:37:09 -0400

    Remove flow_runtime example

--- examples/flow_runtime/hello_world/env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export WXO_SERVER_MSG_THREAD_ID=
--- examples/flow_runtime/hello_world/flow-Tip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-. "$SCRIPT_DIR"/env.sh
-
-curl -X POST -k "http://localhost:9044/tempus/v1/flow/hello_world/version/TIP" -v \
-  --header 'Content-Type: application/json' \
-  --header "wxoMsgThreadId: ${WXO_SERVER_MSG_THREAD_ID}" \
-  --header "Authorization: Bearer $(cat ~/.cache/orchestrate/credentials.yaml  | yq .auth.local.wxo_mcsp_token)"
