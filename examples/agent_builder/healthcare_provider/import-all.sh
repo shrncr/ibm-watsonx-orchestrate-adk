60bae538a84296a30dace00f1ef3c2a2d1e1cb55
Darragh McGonigle <Darragh.McGonigle@ibm.com>
2025-05-02 17:17:21 +0100

    chore(examples); Replace cat example with healthcare (#844)

--- examples/agent_builder/healthcare_provider/import-all.sh
+++ examples/agent_builder/healthcare_provider/import-all.sh
@@ -4,11 +4,11 @@ set -x
 orchestrate env activate local
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for openapi_tool in cat-facts.openapi.yml; do
+for openapi_tool in get-healthcare-providers.yml; do
   orchestrate tools import -k openapi -f ${SCRIPT_DIR}/tools/${openapi_tool} -r ${SCRIPT_DIR}/tools/requirements.txt;
 done
 
-for agent in jokester_agent.yaml; do
+for agent in healthcare_agent.yaml; do
   orchestrate agents import -f ${SCRIPT_DIR}/agents/${agent}
 done
 
